bda15125d61bdf98dc7e0d640faeb6a0bf1dacc2
unknown <godgodx4@gmail.com>
2024-12-27 06:45:47 +0900

    fix webpack option for multi page bundle

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   config: (routes) => {
     return {
       mode: 'development',
-      entry: './src/assets/typescript/index/index.ts',
+      entry: `./src/assets/typescript/${routes}/index.ts`,
       target: 'electron-renderer',
       output: {
         filename: routes + ".js"

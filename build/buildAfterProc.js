10dea5dcad9539b0a31433c9f233d2ec9103f33d
unknown <godgodx4@gmail.com>
2025-11-23 03:13:15 +0900

    add build-lite command

---
+++ build/buildAfterProc.js
@@ -0,0 +1,66 @@
+const fsp = require('fs').promises
+const path = require('path')
+
+const TargetDir = `${__dirname}/../publish.temp`
+const DestDir = `${__dirname}/../publish`
+const RemoveExcludes = [
+  `${DestDir}/win-unpacked/externalPackage`
+]
+
+async function isExistFolder(dir){
+  try{
+    let stat = await fsp.stat(dir)
+
+    return stat.isDirectory()
+  }catch(_){return false}
+}
+
+async function removeDirRecursiveWithExclusive(targetDir, exclusivePaths = []){
+  let targetDirResolve = path.resolve(targetDir).replaceAll('\\', '/')
+  let exclusiveResolvePaths = []
+  let dirsBuff = await fsp.readdir(targetDirResolve, {withFileTypes: true})
+  let resolvePathBuff
+
+  for(let exp of exclusivePaths){
+    exclusiveResolvePaths.push(path.resolve(exp).replaceAll('\\', '/'))
+  }
+
+  for(let d of dirsBuff){
+    resolvePathBuff = path.resolve(`${targetDirResolve}/${d.name}`).replaceAll('\\', '/')
+
+    if(exclusiveResolvePaths.includes(resolvePathBuff)) continue
+    
+    if(d.isDirectory()) await removeDirRecursiveWithExclusive(resolvePathBuff, exclusiveResolvePaths)
+    else await fsp.unlink(resolvePathBuff)
+  }
+
+  dirsBuff = await fsp.readdir(targetDirResolve)
+
+  if(dirsBuff.length == 0) await fsp.rmdir(targetDirResolve)
+}
+
+async function copyAllRecursive(targetDir, destDir){
+  let targetDirResolve = path.resolve(targetDir).replaceAll('\\', '/')
+  let destDirResolve = path.resolve(destDir).replaceAll('\\', '/')
+  let targetDirs = await fsp.readdir(targetDirResolve, {withFileTypes: true})
+  let resolvePathBuff
+
+  for(let d of targetDirs){
+    let resolvePathBuff = path.resolve(`${targetDirResolve}/${d.name}`).replaceAll('\\', '/')
+
+    if(d.isDirectory()) await copyAllRecursive(resolvePathBuff, `${destDirResolve}/${d.name}`)
+    else{
+      await fsp.mkdir(destDirResolve, {recursive: true})
+
+      await fsp.copyFile(`${targetDirResolve}/${d.name}`, `${destDirResolve}/${d.name}`)
+    }
+  }
+}
+
+(async () => {
+  if(!await isExistFolder(DestDir)) await fsp.mkdir(DestDir)
+
+  await removeDirRecursiveWithExclusive(DestDir, RemoveExcludes)
+  await copyAllRecursive(TargetDir, DestDir)
+  await fsp.rmdir(TargetDir, {recursive: true, force: true})
+})()
